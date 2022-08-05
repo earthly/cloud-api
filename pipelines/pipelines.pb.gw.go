c635ebc92c63041112e9b8d6ed2ce474665f3d55
griswoldthecat <griswoldthecat@users.noreply.github.com>
2022-08-05 04:24:29 +0000

    auto-commit via earthly build

--- pipelines/pipelines.pb.gw.go
+++ pipelines/pipelines.pb.gw.go
@@ -398,37 +398,37 @@ func local_request_Pipelines_GetSatellite_0(ctx context.Context, marshaler runti
 }
 
 var (
-	filter_Pipelines_ListGitHubRepos_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+	filter_Pipelines_ListRemoteRepos_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
-func request_Pipelines_ListGitHubRepos_0(ctx context.Context, marshaler runtime.Marshaler, client PipelinesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq ListGitHubReposRequest
+func request_Pipelines_ListRemoteRepos_0(ctx context.Context, marshaler runtime.Marshaler, client PipelinesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ListRemoteReposRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Pipelines_ListGitHubRepos_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Pipelines_ListRemoteRepos_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.ListGitHubRepos(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.ListRemoteRepos(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Pipelines_ListGitHubRepos_0(ctx context.Context, marshaler runtime.Marshaler, server PipelinesServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq ListGitHubReposRequest
+func local_request_Pipelines_ListRemoteRepos_0(ctx context.Context, marshaler runtime.Marshaler, server PipelinesServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ListRemoteReposRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Pipelines_ListGitHubRepos_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Pipelines_ListRemoteRepos_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.ListGitHubRepos(ctx, &protoReq)
+	msg, err := server.ListRemoteRepos(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -577,18 +577,18 @@ func RegisterPipelinesHandlerServer(ctx context.Context, mux *runtime.ServeMux, 
 
 	})
 
-	mux.Handle("GET", pattern_Pipelines_ListGitHubRepos_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Pipelines_ListRemoteRepos_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/api.public.pipelines.Pipelines/ListGitHubRepos", runtime.WithHTTPPathPattern("/api/v0/repos/github"))
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/api.public.pipelines.Pipelines/ListRemoteRepos", runtime.WithHTTPPathPattern("/api/v0/repos/remote"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Pipelines_ListGitHubRepos_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Pipelines_ListRemoteRepos_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -596,7 +596,7 @@ func RegisterPipelinesHandlerServer(ctx context.Context, mux *runtime.ServeMux, 
 			return
 		}
 
-		forward_Pipelines_ListGitHubRepos_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Pipelines_ListRemoteRepos_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -761,23 +761,23 @@ func RegisterPipelinesHandlerClient(ctx context.Context, mux *runtime.ServeMux, 
 
 	})
 
-	mux.Handle("GET", pattern_Pipelines_ListGitHubRepos_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Pipelines_ListRemoteRepos_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/api.public.pipelines.Pipelines/ListGitHubRepos", runtime.WithHTTPPathPattern("/api/v0/repos/github"))
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/api.public.pipelines.Pipelines/ListRemoteRepos", runtime.WithHTTPPathPattern("/api/v0/repos/remote"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Pipelines_ListGitHubRepos_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Pipelines_ListRemoteRepos_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Pipelines_ListGitHubRepos_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Pipelines_ListRemoteRepos_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -797,7 +797,7 @@ var (
 
 	pattern_Pipelines_GetSatellite_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v0", "satellites", "name"}, ""))
 
-	pattern_Pipelines_ListGitHubRepos_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v0", "repos", "github"}, ""))
+	pattern_Pipelines_ListRemoteRepos_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v0", "repos", "remote"}, ""))
 )
 
 var (
@@ -813,5 +813,5 @@ var (
 
 	forward_Pipelines_GetSatellite_0 = runtime.ForwardResponseMessage
 
-	forward_Pipelines_ListGitHubRepos_0 = runtime.ForwardResponseMessage
+	forward_Pipelines_ListRemoteRepos_0 = runtime.ForwardResponseMessage
 )
