62ef735332975b187dcd6f86cc4e6b7aec10f120
littleredcorvette <littleredcorvette@users.noreply.github.com>
2023-03-07 15:27:19 +0000

    auto-commit via earthly build

--- pipelines/pipelines.pb.gw.go
+++ pipelines/pipelines.pb.gw.go
@@ -630,6 +630,74 @@ func local_request_Pipelines_RemoveProjectRepo_0(ctx context.Context, marshaler 
 
 }
 
+func request_Pipelines_UpdateProjectRepoBranch_0(ctx context.Context, marshaler runtime.Marshaler, client PipelinesClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpdateProjectRepoBranchRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["repo_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "repo_id")
+	}
+
+	protoReq.RepoId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "repo_id", err)
+	}
+
+	msg, err := client.UpdateProjectRepoBranch(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Pipelines_UpdateProjectRepoBranch_0(ctx context.Context, marshaler runtime.Marshaler, server PipelinesServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UpdateProjectRepoBranchRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["repo_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "repo_id")
+	}
+
+	protoReq.RepoId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "repo_id", err)
+	}
+
+	msg, err := server.UpdateProjectRepoBranch(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 var (
 	filter_Pipelines_ListProjectRepos_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
@@ -1248,6 +1316,29 @@ func RegisterPipelinesHandlerServer(ctx context.Context, mux *runtime.ServeMux, 
 
 	})
 
+	mux.Handle("POST", pattern_Pipelines_UpdateProjectRepoBranch_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/api.public.pipelines.Pipelines/UpdateProjectRepoBranch", runtime.WithHTTPPathPattern("/api/vo/repos/{repo_id}/reference-branch"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Pipelines_UpdateProjectRepoBranch_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Pipelines_UpdateProjectRepoBranch_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_Pipelines_ListProjectRepos_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1736,6 +1827,26 @@ func RegisterPipelinesHandlerClient(ctx context.Context, mux *runtime.ServeMux, 
 
 	})
 
+	mux.Handle("POST", pattern_Pipelines_UpdateProjectRepoBranch_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/api.public.pipelines.Pipelines/UpdateProjectRepoBranch", runtime.WithHTTPPathPattern("/api/vo/repos/{repo_id}/reference-branch"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Pipelines_UpdateProjectRepoBranch_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Pipelines_UpdateProjectRepoBranch_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_Pipelines_ListProjectRepos_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1944,6 +2055,8 @@ var (
 
 	pattern_Pipelines_RemoveProjectRepo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "v0", "repos", "id"}, ""))
 
+	pattern_Pipelines_UpdateProjectRepoBranch_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"api", "vo", "repos", "repo_id", "reference-branch"}, ""))
+
 	pattern_Pipelines_ListProjectRepos_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v0", "repos"}, ""))
 
 	pattern_Pipelines_ListRemotePipelines_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v0", "pipelines", "remote"}, ""))
@@ -1987,6 +2100,8 @@ var (
 	forward_Pipelines_AddProjectRepos_0 = runtime.ForwardResponseMessage
 
 	forward_Pipelines_RemoveProjectRepo_0 = runtime.ForwardResponseMessage
+
+	forward_Pipelines_UpdateProjectRepoBranch_0 = runtime.ForwardResponseMessage
 
 	forward_Pipelines_ListProjectRepos_0 = runtime.ForwardResponseMessage
 
--- pipelines/pipelines_grpc.pb.go
+++ pipelines/pipelines_grpc.pb.go
@@ -51,6 +51,9 @@ type PipelinesClient interface {
 	AddProjectRepos(ctx context.Context, in *AddProjectReposRequest, opts ...grpc.CallOption) (*AddProjectReposResponse, error)
 	// RemoveProjectRepo removes a repository from a project.
 	RemoveProjectRepo(ctx context.Context, in *RemoveProjectRepoRequest, opts ...grpc.CallOption) (*RemoveProjectRepoResponse, error)
+	// UpdateProjectRepoBranch modifies the reference_branch for a project repository
+	// This is a dedicated RPC because changing the definition_branch will cause a resync of pipelines (potentially destructive)
+	UpdateProjectRepoBranch(ctx context.Context, in *UpdateProjectRepoBranchRequest, opts ...grpc.CallOption) (*UpdateProjectRepoBranchResponse, error)
 	// ListProjectRespos lists all project repositories.
 	ListProjectRepos(ctx context.Context, in *ListProjectReposRequest, opts ...grpc.CallOption) (*ListProjectReposResponse, error)
 	// ListRemotePipelines uses the GitHub API to list pipeline definitions present in a remote repository.
@@ -210,6 +213,15 @@ func (c *pipelinesClient) RemoveProjectRepo(ctx context.Context, in *RemoveProje
 	return out, nil
 }
 
+func (c *pipelinesClient) UpdateProjectRepoBranch(ctx context.Context, in *UpdateProjectRepoBranchRequest, opts ...grpc.CallOption) (*UpdateProjectRepoBranchResponse, error) {
+	out := new(UpdateProjectRepoBranchResponse)
+	err := c.cc.Invoke(ctx, "/api.public.pipelines.Pipelines/UpdateProjectRepoBranch", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *pipelinesClient) ListProjectRepos(ctx context.Context, in *ListProjectReposRequest, opts ...grpc.CallOption) (*ListProjectReposResponse, error) {
 	out := new(ListProjectReposResponse)
 	err := c.cc.Invoke(ctx, "/api.public.pipelines.Pipelines/ListProjectRepos", in, out, opts...)
@@ -324,6 +336,9 @@ type PipelinesServer interface {
 	AddProjectRepos(context.Context, *AddProjectReposRequest) (*AddProjectReposResponse, error)
 	// RemoveProjectRepo removes a repository from a project.
 	RemoveProjectRepo(context.Context, *RemoveProjectRepoRequest) (*RemoveProjectRepoResponse, error)
+	// UpdateProjectRepoBranch modifies the reference_branch for a project repository
+	// This is a dedicated RPC because changing the definition_branch will cause a resync of pipelines (potentially destructive)
+	UpdateProjectRepoBranch(context.Context, *UpdateProjectRepoBranchRequest) (*UpdateProjectRepoBranchResponse, error)
 	// ListProjectRespos lists all project repositories.
 	ListProjectRepos(context.Context, *ListProjectReposRequest) (*ListProjectReposResponse, error)
 	// ListRemotePipelines uses the GitHub API to list pipeline definitions present in a remote repository.
@@ -384,6 +399,9 @@ func (UnimplementedPipelinesServer) AddProjectRepos(context.Context, *AddProject
 }
 func (UnimplementedPipelinesServer) RemoveProjectRepo(context.Context, *RemoveProjectRepoRequest) (*RemoveProjectRepoResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RemoveProjectRepo not implemented")
+}
+func (UnimplementedPipelinesServer) UpdateProjectRepoBranch(context.Context, *UpdateProjectRepoBranchRequest) (*UpdateProjectRepoBranchResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateProjectRepoBranch not implemented")
 }
 func (UnimplementedPipelinesServer) ListProjectRepos(context.Context, *ListProjectReposRequest) (*ListProjectReposResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ListProjectRepos not implemented")
@@ -644,6 +662,24 @@ func _Pipelines_RemoveProjectRepo_Handler(srv interface{}, ctx context.Context, 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Pipelines_UpdateProjectRepoBranch_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateProjectRepoBranchRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PipelinesServer).UpdateProjectRepoBranch(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/api.public.pipelines.Pipelines/UpdateProjectRepoBranch",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PipelinesServer).UpdateProjectRepoBranch(ctx, req.(*UpdateProjectRepoBranchRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _Pipelines_ListProjectRepos_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ListProjectReposRequest)
 	if err := dec(in); err != nil {
@@ -856,6 +892,10 @@ var Pipelines_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "RemoveProjectRepo",
 			Handler:    _Pipelines_RemoveProjectRepo_Handler,
+		},
+		{
+			MethodName: "UpdateProjectRepoBranch",
+			Handler:    _Pipelines_UpdateProjectRepoBranch_Handler,
 		},
 		{
 			MethodName: "ListProjectRepos",
