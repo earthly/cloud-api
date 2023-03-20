3ca99a5dcf4f893abe16456a054a7525cbb5623f
littleredcorvette <littleredcorvette@users.noreply.github.com>
2023-03-20 02:27:42 +0000

    auto-commit via earthly build

--- logstream/logstream.pb.gw.go
+++ logstream/logstream.pb.gw.go
@@ -74,9 +74,20 @@ func request_LogStream_StreamLogs_0(ctx context.Context, marshaler runtime.Marsh
 	return stream, metadata, nil
 }
 
+var (
+	filter_LogStream_GetFirebaseAuthToken_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_LogStream_GetFirebaseAuthToken_0(ctx context.Context, marshaler runtime.Marshaler, client LogStreamClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetFirebaseAuthTokenRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_LogStream_GetFirebaseAuthToken_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.GetFirebaseAuthToken(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -86,6 +97,13 @@ func request_LogStream_GetFirebaseAuthToken_0(ctx context.Context, marshaler run
 func local_request_LogStream_GetFirebaseAuthToken_0(ctx context.Context, marshaler runtime.Marshaler, server LogStreamServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetFirebaseAuthTokenRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_LogStream_GetFirebaseAuthToken_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.GetFirebaseAuthToken(ctx, &protoReq)
 	return msg, metadata, err
