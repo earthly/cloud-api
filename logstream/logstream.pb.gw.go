784d1107647942408b88db44cfb234c2cd40a52e
littleredcorvette <littleredcorvette@users.noreply.github.com>
2023-08-28 21:02:57 +0000

    auto-commit via earthly build

--- logstream/logstream.pb.gw.go
+++ logstream/logstream.pb.gw.go
@@ -309,7 +309,7 @@ func local_request_LogStream_LongTermExists_0(ctx context.Context, marshaler run
 
 }
 
-func request_LogStream_GetLongTerm_0(ctx context.Context, marshaler runtime.Marshaler, client LogStreamClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func request_LogStream_GetLongTerm_0(ctx context.Context, marshaler runtime.Marshaler, client LogStreamClient, req *http.Request, pathParams map[string]string) (LogStream_GetLongTermClient, runtime.ServerMetadata, error) {
 	var protoReq GetLongTermRequest
 	var metadata runtime.ServerMetadata
 
@@ -330,34 +330,16 @@ func request_LogStream_GetLongTerm_0(ctx context.Context, marshaler runtime.Mars
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "build_id", err)
 	}
 
-	msg, err := client.GetLongTerm(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_LogStream_GetLongTerm_0(ctx context.Context, marshaler runtime.Marshaler, server LogStreamServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq GetLongTermRequest
-	var metadata runtime.ServerMetadata
-
-	var (
-		val string
-		ok  bool
-		err error
-		_   = err
-	)
-
-	val, ok = pathParams["build_id"]
-	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "build_id")
-	}
-
-	protoReq.BuildId, err = runtime.String(val)
+	stream, err := client.GetLongTerm(ctx, &protoReq)
 	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "build_id", err)
+		return nil, metadata, err
 	}
-
-	msg, err := server.GetLongTerm(ctx, &protoReq)
-	return msg, metadata, err
+	header, err := stream.Header()
+	if err != nil {
+		return nil, metadata, err
+	}
+	metadata.HeaderMD = header
+	return stream, metadata, nil
 
 }
 
@@ -507,28 +489,10 @@ func RegisterLogStreamHandlerServer(ctx context.Context, mux *runtime.ServeMux, 
 	})
 
 	mux.Handle("GET", pattern_LogStream_GetLongTerm_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/api.public.logstream.LogStream/GetLongTerm", runtime.WithHTTPPathPattern("/api/v0/logstream/long-term/{build_id}"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_LogStream_GetLongTerm_0(annotatedContext, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_LogStream_GetLongTerm_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
+		err := status.Error(codes.Unimplemented, "streaming calls are not yet supported in the in-process transport")
+		_, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+		return
 	})
 
 	return nil
@@ -744,7 +708,7 @@ func RegisterLogStreamHandlerClient(ctx context.Context, mux *runtime.ServeMux, 
 			return
 		}
 
-		forward_LogStream_GetLongTerm_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_LogStream_GetLongTerm_0(annotatedContext, mux, outboundMarshaler, w, req, func() (proto.Message, error) { return resp.Recv() }, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -784,5 +748,5 @@ var (
 
 	forward_LogStream_LongTermExists_0 = runtime.ForwardResponseMessage
 
-	forward_LogStream_GetLongTerm_0 = runtime.ForwardResponseMessage
+	forward_LogStream_GetLongTerm_0 = runtime.ForwardResponseStream
 )
